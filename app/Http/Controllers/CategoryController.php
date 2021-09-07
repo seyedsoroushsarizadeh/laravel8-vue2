61130d1446ce83676ac70f6ff4e58f33639e9e1f
seyed soroush sarizadeh <seyed.soroush.sarizadeh@gmail.com>
2021-09-07 22:08:53 +0430

    laravel-vue-and-api

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all(['id','title','description']);
+        return response()->json($categories);
+    }
+
+    /**
+     * Store a newly created resource in storages
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->post());
+        return response()->json([
+            'message'=>'Category Created Successfully!!',
+            'category'=>$category
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->fill($request->post())->save();
+        return response()->json([
+            'message'=>'Category Updated Successfully!!',
+            'category'=>$category
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json([
+            'message'=>'Category Deleted Successfully!!'
+        ]);
+    }
+}
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>TechvBlogs</h1>
+            <a href="https://techvblogs.com?utm_source=blogExampleRepo" target="_blank">Visit For More Blogs</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
